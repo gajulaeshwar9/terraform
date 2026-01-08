21e62ae6624b022510d6009ad4691808b377249f
Gajula Eshwar <gajulaeshwar9@gmail.com>
2026-01-09 01:26:40 +0530

    Update AMI ID for aws_instance resource

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "three" {
   count         = 1
-  ami           = "ami-068c0051b15cdb816"
+  ami           = "ami-07ff62358b87c7116"
   instance_type = "t3.micro"
 
   tags = {
