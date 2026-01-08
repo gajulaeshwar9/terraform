12d933850ad97aa99e6dfba35788e10fbb0d6ad8
Gajula Eshwar <gajulaeshwar9@gmail.com>
2026-01-09 01:30:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 
-resource "aws_instance" "three" {
+resource "aws_instance" "two" {
   count         = 1
   ami           = "ami-07ff62358b87c7116"
   instance_type = "t3.micro"
